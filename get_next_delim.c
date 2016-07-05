9c1ae4f9bface6e32ac8ffc2af4e44025ed411a7
Leo <leiyoo.42@gmail.com>
2016-07-05 14:52:04 +0000

    get_next_delim

---
+++ get_next_delim.c
@@ -0,0 +1,59 @@
+#include "libft.h"
+#include "get_next_delim.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+static void		gnd_extract_content(char **stock, char **line, char *delim,
+									size_t size_delim)
+{
+	size_t	size;
+	char	*ptr;
+
+	ptr = *stock;
+	size = (size_t)delim - (size_t)*stock;
+	*line = ft_strndup(*stock, size);
+	*stock = ft_strdup(delim + size_delim);
+	ft_strdel(&ptr);
+}
+
+static int		gnd_read(const int fd, char **stock)
+{
+	size_t			size;
+	char			buf[BUF_SIZE + 1];
+	char			*ptr;
+
+	ft_bzero(buf, BUF_SIZE + 1);
+	ptr = *stock;
+	size = read(fd, buf, BUF_SIZE);
+	*stock = ft_strjoin(*stock, buf);
+	ft_strdel(&ptr);
+	return (size);
+}
+
+int		get_next_delim(const int fd, char **line, const char *delim)
+{
+	static char		*stock = NULL;
+	int				value;
+	char			*ptr;
+
+	value = 0;
+	if (stock == NULL)
+		stock = ft_strnew(0);
+	if ((ptr = ft_strstr(stock, delim)) != NULL)
+		gnd_extract_content(&stock, line, ptr, ft_strlen(delim));
+	else
+	{
+		while ((value = gnd_read(fd, &stock)) > 0)
+		{
+			if ((ptr = ft_strstr(stock, delim)) != NULL)
+			{
+				gnd_extract_content(&stock, line, ptr, ft_strlen(delim));
+				return (value);
+			}
+		}
+		*line = ft_strdup(stock);
+		ft_strdel(&stock);
+		value = -1;
+	}
+	return (value);
+}
---
+++ get_next_delim.h
@@ -0,0 +1,8 @@
+#ifndef GET_NEXT_DELIM_H
+# define GET_NEXT_DELIM_H
+
+# define BUF_SIZE 1
+
+int		get_next_delim(const int fd, char **line, const char *delim);
+
+#endif
