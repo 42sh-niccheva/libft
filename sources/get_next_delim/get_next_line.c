0cc9354470fb9c9beae227e84850e6a963678295
Leo Lapillonne <llapillo@student.42.fr>
2016-07-07 10:57:37 +0200

    save 4octets

--- sources/get_next_delim/get_next_line.c
+++ sources/get_next_delim/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 10:16:45 by llapillo          #+#    #+#             */
-/*   Updated: 2016/07/07 10:32:35 by llapillo         ###   ########.fr       */
+/*   Updated: 2016/07/07 10:56:34 by llapillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,5 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	int		value;
-
-	value = 0;
-	value = get_next_delim(fd, line, "\n");
-	return (value);
+	return (get_next_delim(fd, line, "\n"));
 }
--- sources/get_next_delim/get_next_word.c
+++ sources/get_next_delim/get_next_word.c
@@ -6,7 +6,7 @@
 /*   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/07 10:16:45 by llapillo          #+#    #+#             */
-/*   Updated: 2016/07/07 10:32:33 by llapillo         ###   ########.fr       */
+/*   Updated: 2016/07/07 10:57:03 by llapillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,5 @@
 
 int		get_next_word(const int fd, char **line)
 {
-	int		value;
-
-	value = 0;
-	value = get_next_delim(fd, line, " ");
-	return (value);
+	return (get_next_delim(fd, line, " "));
 }
